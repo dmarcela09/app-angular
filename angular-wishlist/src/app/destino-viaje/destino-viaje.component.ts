03730352cf29c75747a90e2a2de308fe942805f8
Dalia Muñoz <dmarcela.15@gmail.com>
2020-07-13 19:24:06 -0500

    adding comments to code

--- angular-wishlist/src/app/destino-viaje/destino-viaje.component.ts
+++ angular-wishlist/src/app/destino-viaje/destino-viaje.component.ts
@@ -12,12 +12,12 @@ export class DestinoViajeComponent implements OnInit {
   //una variable con especificacion de tipo de valor, pasar a constructor
   @Input() destinos: DestinoViaje; //antes "nombre: string;"
   //nombre es susceptible de ser pasado como parametro con el decorador '@Input'
+
   //nueva directiva para corregir espacio en el html, conexion directa con el atributo
   @HostBinding('attr.class') cssClass = 'col-md-4'; 
 
   constructor() { 
-    //this.nombre = 'nombre por defecto';
-    //inicializa variable, esta es llamada en el html con {{ }}
+
   }
 
   ngOnInit(): void{
--- angular-wishlist/src/app/lista-destinos/lista-destinos.component.ts
+++ angular-wishlist/src/app/lista-destinos/lista-destinos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-//Importar clase DestinoViaje en la carpeta models
+//Importar clase DestinoViaje desde la carpeta models
 import { DestinoViaje } from './../models/destino-viaje.models';
 
 @Component({
@@ -9,12 +9,9 @@ import { DestinoViaje } from './../models/destino-viaje.models';
 })
 export class ListaDestinosComponent implements OnInit {
 //Para la lista se crea un array(vector, lista de elementos)
- //destinos: string[]; primero
  destinos: DestinoViaje[];
 
   constructor() { 
-    //se listan los valores del array
-    //this.destinos = ['Barcelona', 'Buenos Aires', 'Lima', 'Cartagena']; primero
     this.destinos = [];
   }
 
@@ -22,9 +19,6 @@ export class ListaDestinosComponent implements OnInit {
   }
 //metodo que guarda los datos desde el formulario por medio de variables de plantilla "#" 
   guardar(nombre:string, url:string):boolean {
-    //console.log(nombre); primero
-    //console.log(url); primero
-    //console.log(new DestinoViaje(nombre, url)); segundo
     this.destinos.push(new DestinoViaje(nombre, url))
     //console.log(this.destinos); para probar la entrada de datos al array
     return false;
